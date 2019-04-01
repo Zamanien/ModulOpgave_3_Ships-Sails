438fcfa6ebfc3bf646c6460a948cb6e382435d0d
Marcus Miyalys <resolutions@riseup.net>
2019-04-01 11:54:11 +0200

    Added the project

---
+++ code/src/main/java/kea/shipsandsails/ShipsandsailsApplication.java
@@ -0,0 +1,13 @@
+package kea.shipsandsails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipsandsailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipsandsailsApplication.class, args);
+	}
+
+}
