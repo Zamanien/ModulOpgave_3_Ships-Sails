73cbb472597308397753a0276ff1257e4b7c43eb
Marcus Miyalys <resolutions@riseup.net>
2019-04-14 00:36:48 +0200

    A ton of updates. Starting to redo the game layout a bit

--- code/src/main/resources/static/js/js.js
+++ code/src/main/resources/static/js/js.js
@@ -2,15 +2,30 @@
 // TODO: Programatically insert all ships from the array, rather than adding them manually in HTML and binding to them here
 // Ships need an ID as Java needs to be able to refer to it uniquely, and it cannot do so based off orientation or position as several ships of the same type may occupy the same space (after a collision).
 class Ship {
-  constructor(el, id, x, y, orientation, type) {
+  constructor(el, id, x, y, orientation, type, nationality) {
     this.el = el
     this.id = id
     this.x = x
     this.y = y
-    this.orientation = orientation
     this.type = type
+
+    this.orientation = orientation // Scenario determined: x, y, type, orientation, nationality. More?
+    this.nationality = nationality
+
+    // Stats
+    
+    // Hardcoded rather than passed in by the constructor?
+    this.sailors     = 100  // In Java this should simply be derived off ship type. Unlike Java it's a percentage here, since it's represented by a progress bar. That could be changed if need be, and then the value must simply be converted to percent for that progress bar.
+    this.hull_health = 100
+    this.sail_health = 100
+    this.currentAmmunitionType = 0
+    this.load = 0
+    this.speed = 0 // The current speed of the ship
   }
 }
+
+// Type stats:
+// max speed, max sail, number_of_sailors_per_sail
 
 class Tile {
   constructor(el, x, y) {
@@ -22,10 +37,16 @@ class Tile {
 
 // Just a few test ships
 // This is supposed to be what the ships look like when we receive them from Java. The element reference is missing at this point as it's generated in JS, say in place_ships.
-const ship1 = new Ship( null, 1, 1, 5, 30, "ship-of-the-line" )
-const ship2 = new Ship( null, 2, 1, 3, 30, "ship-of-the-line" )
+const ship1 = new Ship( null, 1, 1, 5, 30, "ship-of-the-line", "Aztec")
+const ship2 = new Ship( null, 2, 1, 3, 30, "brig", "Aztec" )
+const ship3 = new Ship( null, 3, 3, 4, 330, "man-at-war", "Aztec" )
+const ship4 = new Ship( null, 5, 1, 7, 330, "man-at-war", "Celts" ) // The enemy!
 
-ships = [ship1, ship2]
+ships = [ship1, ship2, ship3, ship4]
+
+// TODO: Hardcoded which nationality the player is for now, just for testing
+player = "Aztec"
+enemy = "Celts"
 
 
 
@@ -43,9 +64,9 @@ var sfx_volume = 0.6
 
 /* Tiles / World settings */
 
-const width = 5                // 4 lower than the number of columns to make The number of columns to make
-const height = 3               // 1 lower than the total number of tiles from top to bottom
-const t_width = width + 3      // Total width and heights, starting from 0
+const width = 7                // 4 lower than the number of columns to make The number of columns to make
+const height = 5               // 1 lower than the total number of tiles from top to bottom
+const t_width = width + 5      // Total width and heights, starting from 0
 
 /* Ship settings */
 
@@ -63,10 +84,20 @@ orientations = [o_offset, o_offset + (o_degrees*1), o_offset + (o_degrees*2), o_
 
 /* SFX settings */
 
-const base_path = 'audio/sfx/wc2v3/'
-//const sounds = ['human1.wav', 'human2.wav', 'human3.wav', 'acknowledge1.wav', 'acknowledge2.wav', 'acknowledge3.wav']
-const sounds_ready = ['Hshpredy.wav', 'Hshpwht1.wav', 'Hshpwht2.wav', 'Hshpwht3.wav']
-const sounds_affirmative = ['Hshpyes1.wav', 'Hshpyes2.wav', 'Hshpyes3.wav']
+const base_path               = 'audio/sfx/'
+const base_path_brig          = 'audio/sfx/brig/'
+const base_path_sotl          = 'audio/sfx/ship-of-the-line/'
+const base_path_maw           = 'audio/sfx/man-at-war/'
+
+const brig_sounds_ready       = ['Kuwhat1.wav', 'Kuwhat2.wav', 'Kuwhat3.wav']
+const brig_sounds_affirmative = ['Kuyessr1.wav', 'Kuyessr2.wav', 'Kuyessr3.wav']
+const sotl_sounds_ready       = ['Hshpredy.wav', 'Hshpwht1.wav', 'Hshpwht2.wav', 'Hshpwht3.wav']
+const sotl_sounds_affirmative = ['Hshpyes1.wav', 'Hshpyes2.wav', 'Hshpyes3.wav']
+const maw_sounds_ready       = ['Alwhat1.wav', 'Alwhat2.wav', 'Alwhat3.wav']
+const maw_sounds_affirmative = ['Alyessr1.wav', 'Alyessr2.wav', 'Alyessr3.wav']
+
+const sounds_sink             = ['ship-sinking.wav']
+const sounds_edge             = ['edge-scream.wav']
 
 
 
@@ -123,14 +154,9 @@ function place_tiles() {
     }
 
   }
-
-  // All these position: absolute element break out of the regular positioning flow and thus the parent element is much smaller than its children.
-  // We can then either hardcode a height for the container that doesn't work if we change the number of rows of tiles, OR we can set the parent container's height via JS:
-  custom_adjustment = -80
-  tilesHTML.style.height =  (height + 2) * tile_offset_height + custom_adjustment + "px"
 }
 
-// Maybe just for debugging
+// Maybe just for debugging, doesn't quite position the numbers correctly, but it's almost good enough for debug
 function place_tile_coordinates(tile_h, tile_w, x, y) {
   let tilesHTML = document.getElementById('tiles') // Get a reference to what will be the element surrounding all the tiles
   let div = document.createElement('div')
@@ -148,12 +174,13 @@ function place_ships() {
   let shipsHTML = document.getElementById('ships')
   for (aShip of ships) {
     let div = document.createElement('div')
+    div.classList.add('ship')       // Adding the styling all ships have in common
     div.classList.add(aShip.type)   // I need to add a class so CSS knows which ship model it for selecting an image to show
     shipsHTML.appendChild(div)
     aShip.el = div // A reference to this particular ship is saved. TODO: Is this even possible?
 
     // Rotate the ship properly 
-    aShip.el.style.transform = "rotate(+" + o_offset + "deg)"; // TODO: The default rotation may vary per ship? Randomize it based off the orientations array? Basically this should be default_rotation + aShip.orientation
+    aShip.el.style.transform = "rotate(+" + aShip.orientation + "deg)"; // TODO: The default rotation may vary per ship? Randomize it based off the orientations array? Basically this should be default_rotation + aShip.orientation
 
     // Put the ship in its proper position on the map
     for (aTile of tiles) {
@@ -172,14 +199,22 @@ function place_ships() {
 
 
 function play_sfx_ready() {
-  play_sfx(sounds_ready)
+  if (ship.type == 'brig') play_sfx(base_path_brig, brig_sounds_ready)
+  else if (ship.type == 'ship-of-the-line') play_sfx(base_path_sotl, sotl_sounds_ready)
+  else if (ship.type == 'man-at-war') play_sfx(base_path_maw, maw_sounds_ready)
 }
 
-function play_sfx(sounds) {
+function play_sfx_affirmative() {
+  if (ship.type == 'brig') play_sfx(base_path_brig, brig_sounds_affirmative)
+  else if (ship.type == 'ship-of-the-line') play_sfx(base_path_sotl, sotl_sounds_affirmative)
+  else if (ship.type == 'man-at-war') play_sfx(base_path_maw, maw_sounds_affirmative)
+}
+
+function play_sfx(bp, sounds) {
 
   if (!sfx_muted) {
 
-    sfx = new Audio(base_path + sounds[ Math.floor(Math.random()*sounds.length) ])
+    sfx = new Audio(bp + sounds[ Math.floor(Math.random()*sounds.length) ])
     sfx.volume = sfx_volume
     sfx.play()
   }
@@ -198,34 +233,41 @@ function music_toggle() {
 }
 
   
+// TODO: Prevent ending the turn if there's moves you have to make left, due to speed
 function end_turn() {
   alert('Ending turn...')
-}
-
-function attack() {
-  alert('Attacking...')
+  if (ship.load != 0) ship.load--
+  display_current_ammunition() // The ammunition might be loaded now, so the UI should show it
 }
 
 // Adding an event listener on the Ships div, used for ship selection below 
 document.getElementById('ships').addEventListener('click', function(event) {
   ship_selection(event.target);
-});
+})
 
 // TODO: Ideally this border should not rotate with the ship. Tried adding a surrounding div and putting a border on that but it was somewhere else (so i guess it would have to be moved as well, just not rotated?)
 // Also call functions for updating the turn/move buttons based off remaining moves/turns (request the info from Java)
 function ship_selection(clicked_ship) {
   // Now we have the ship element. Find the related ship object:
   for (aShip of ships) {
-    if (aShip.el == clicked_ship) { 
+    // Only allow the user to select the ships of their own nationality
+    if (aShip.el == clicked_ship && aShip.nationality == player) { 
       ship = aShip
       aShip.el.classList.add('ship-selected')
+
+      /* Update health bars */
+      document.getElementById('sailors').style.width = ship.sailors + "%"
+      document.getElementById('hull-health').style.width = ship.hull_health + "%"
+      document.getElementById('sails-health').style.width = ship.sail_health + "%"
+
+      display_current_ammunition() // Update the UI to show the currently loaded ammunition for the current ship
+
+      play_sfx_ready()
     }
     else {
-      aShip.el.classList.remove('ship-selected')
+      aShip.el.classList.remove('ship-selected') // Remove selected from all other ships
     }
   }
-  play_sfx_ready()
-  // Maybe I can just delete the "selected" attribute and simply do one thing: assign the global ship variable to whichever ship is the current one?
 }
 
 
@@ -264,6 +306,7 @@ function move() {
     }
 
     if (ship.x < 0 || ship.y < 0 || ship.x > height || ship.y > t_width) move_over_edge()
+    play_sfx_affirmative()
 
     // Moving based off updated coordinates. Almost verbatim copy of the "placing ships algorithm" (changed aShip to ship), so if all this works I should extract this to a method
     for (aTile of tiles) {
@@ -275,34 +318,31 @@ function move() {
         break
       }
     }
-
-  play_sfx(sounds_affirmative)
 }
 
-// Old move method, useful for moving outside of tiles, such as over the edge. TODO on 4 of the directions, though
+// Old move method, useful for moving outside of tiles, such as over the edge.
 function move_over_edge() {
   // We need to move it first for visuals' sake, but this is helpful for faster debugging
   ship.el.parentElement.removeChild(ship.el) 
   ship = null
 
-  // Get current position
-  curPosLeft = parseInt(ship.el.style.left)
-  curPosTop = parseInt(ship.el.style.top)
+  // // Get current position
+  // curPosLeft = parseInt(ship.el.style.left)
+  // curPosTop = parseInt(ship.el.style.top)
 
-  orientation = ship.el.style.transform
+  // // Currently it moves left and up equally, which makes sense at 45 degree angles, but we're using 30 degree angles so it should move further on the x-axis than the y-axis
+  // // Calculate the move factoring in angles, like base_move * the distance from the nearest 90 degree angle, and base_distance could be 3 or something? Say (90-30) * 3 and (90-60) * 3?
+  // if (ship.orientation == 30) {
+  //   ship.el.style.left = (curPosLeft - ship_move_distance) + "px"
+  //   ship.el.style.top = (curPosTop - ship_move_distance) + "px"
+  // }
+  // else if (ship.orientation == 330) {
+  //   ship.el.style.left = (curPosLeft - ship_move_distance) + "px"
+  //   ship.el.style.top = (curPosTop + ship_move_distance) + "px"
+  // }
 
-  // TODO: Currently it moves left and up equally, which makes sense at 45 degree angles, but we're using 30 degree angles so it should move further on the x-axis than the y-axis
-  // Calculate the move factoring in angles, like base_move * the distance from the nearest 90 degree angle, and base_distance could be 3 or something? Say (90-30) * 3 and (90-60) * 3?
-  if (orientation == "rotate(30deg)") {
-    ship.el.style.left = (curPosLeft - ship_move_distance) + "px"
-    ship.el.style.top = (curPosTop - ship_move_distance) + "px"
-  }
-  else if (orientation == "rotate(-30deg)") {
-    ship.el.style.left = (curPosLeft - ship_move_distance) + "px"
-    ship.el.style.top = (curPosTop + ship_move_distance) + "px"
-  }
-
-  play_sfx(sounds_affirmative)
+  //play_sfx(base_path, sounds_sink)
+  play_sfx(base_path, sounds_edge)
 }
 
 // TURNING
@@ -319,17 +359,11 @@ function turn_clockwise() {
 
 
 function turn_counter_clockwise() {
-  // array_offset = orientations.indexOf(cur_orientation)
-  // if (array_offset == 0) offset = 5
-  // else offset = array_offset - 1
 
   if (ship.orientation != 30) ship.orientation -= o_degrees
   else ship.orientation = 330
 
   turn_visual("CCW")
-  // Handle reflect and changing the image here.
-  //ship.el.style.backgroundPosition = '0px -300px';
-  //ship.el.classList.remove('ship-right')
 }
 
 
@@ -343,17 +377,123 @@ function turn_visual(direction) {
   else {
     ship.el.style.transform = 'rotate(' + (cur_orientation - o_degrees) + 'deg)'
   }
-  //   ship.el.classList.add('ship-right')
+
+  // Handling image switching here:
+  clear_rotation_classes();
+  if (ship.orientation == 30) {   
+    ship.el.classList.add('ship-left')
+    ship.el.style.transition = 'transform 2s'
+    ship.el.style.transform = 'rotate(30deg)'
+  }
+  else if (ship.orientation == 330)    {
+    ship.el.classList.add('ship-left')
+    ship.el.style.transition = 'transform 2s'
+    ship.el.style.transform = 'rotate(-30deg)'
+  }
+  else if (ship.orientation == 150) {
+    ship.el.classList.add('ship-right')
+    ship.el.style.transition = 'transform 2s'
+    ship.el.style.transform = 'rotate(-30deg)'
+  }
+  else if (ship.orientation == 210) {
+    ship.el.classList.add('ship-right')
+    ship.el.style.transition = 'transform 2s'
+    ship.el.style.transform = 'rotate(30deg)'
+  }
+
+  else if (ship.orientation == 90)   { 
+    ship.el.classList.add('ship-up')
+    ship.el.style.transition = 'transform 0s'
+    ship.el.style.transform = 'rotate(0deg)' // Set to 0 rather than empty string to not mess up parseInt above
+  }
+  else { // 270  
+    ship.el.classList.add('ship-down')
+    ship.el.style.transition = 'transform 0s'
+    ship.el.style.transform = 'rotate(0)'
+  }
+  ship.el.style.transition = 'top 1.7s, left 1.7s'; /* Also reset by setting transition just to transform 0s, so... */
+}
+
+function clear_rotation_classes() {
+  ship.el.classList.remove('ship-left')
+  ship.el.classList.remove('ship-right')
+  ship.el.classList.remove('ship-up')
+  ship.el.classList.remove('ship-down')
 }
 
 
+/* ATTACK / WEAPONS */
+
+const ball  = document.getElementById('change-ammunition-ball')
+const chain = document.getElementById('change-ammunition-chain')
+const grape = document.getElementById('change-ammunition-grape')
+const CANNON = 0
+const CHAIN  = 1
+const GRAPE  = 2
+
+// TODO: Implement attacking here. We also need support for the player clicking where to attack on the map/tiles, after clicking the attack button.
+function attack() {
+  if (ship.load == 0) {
+    alert('Attacking...')
+    ship.load = 2 // It will be decremented by 1 at the start of the next round, making it reach 0 after two "end turns"
+    display_current_ammunition() // Update this as ship.load is now above 0, no longer ready to fire
+  }
+  else alert("Your current weapon isn't loaded. You have to wait at least " + ship.load + " turns.")
+}
+
+// TODO: Add turn-based delays to this
+function update_ammunition(ammo_clicked) {
+
+  if (ship.load == 0) {
+    alert("The cannon is already loaded. Fire it to be able to reload.")
+    return
+  }
+  else if (ship.currentAmmunitionType == ammo_clicked) return // Don't do anything if clicking the currently selected ammunition
+  else {
+
+    ship.load = 3 // It will be decremented by 1 at the start of the next round, making it reach 0 after three "end turns"
+
+    ship.currentAmmunitionType = ammo_clicked
+    display_current_ammunition() // Now display the updated current ammunition
+  }
+}
+
+function display_current_ammunition() {
+  switch (ship.currentAmmunitionType) {
+    case CANNON: 
+      current = ball
+      ds1 = chain; ds2 = grape
+      break
+    case CHAIN: 
+      current = chain
+      ds1 = ball; ds2 = grape
+      break
+    case GRAPE: 
+      current = grape
+      ds1 = ball; ds2 = chain
+      break
+  }
+  // Remove the current classes from all ammunition types:
+  // Obviously only one can be selected at a time, and as for the active one maybe it already has red and green, where red was added last, and then it will stay red after trying to add the green class, even though the weapon has reloaded.
+  ball.classList.remove('text-success', 'text-danger') 
+  chain.classList.remove('text-success', 'text-danger')
+  grape.classList.remove('text-success', 'text-danger')
+
+  cl = (ship.load == 0)? 'text-success' : 'text-danger'
+  current.classList.add(cl)
+}
+
+
+ball.onclick  = () => { update_ammunition(CANNON) }
+
+chain.onclick = () => { update_ammunition(CHAIN) }
+grape.onclick = () => { update_ammunition(GRAPE) }
+
+document.getElementById('attack').onclick = attack
 
 
 
 /* EVENT BINDINGS */
-
-// TODO: This should fire regardless of which ship is clicked? May be handled in method further up
-// ship.el.onclick = play_sfx_ready
 
 document.getElementById('music-button').onclick = music_toggle
 document.getElementById('sfx-button').onclick = sfx_toggle
@@ -363,4 +503,3 @@ document.getElementById('end-turn').onclick = end_turn
 document.getElementById('move-arrow').onclick = move
 document.getElementById('turn-clockwise').onclick = turn_clockwise
 document.getElementById('turn-counter-clockwise').onclick = turn_counter_clockwise
-document.getElementById('attack').onclick = attack
